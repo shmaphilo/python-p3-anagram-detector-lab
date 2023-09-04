ccafbb742ecd02e82693246f09d06c2ad5e04415
shmaphilo <philipogaye062@gmail.com>
2023-09-04 01:36:28 -0400

    Assignment Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        result = []
+        sorted_word = sorted(self.word.lower())
+
+        for candidate in word_list:
+            if sorted_word == sorted(candidate.lower()) and self.word.lower() != candidate.lower():
+                result.append(candidate)
+
+        return result
